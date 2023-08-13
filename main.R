9b0ab43a98ce57f23fdf7ab24bde86e1922af8ad
Johann Freitas <johannmarques461@gmail.com>
2023-08-13 20:09:22 -0300

    Finishing plot

--- main.R
+++ main.R
@@ -2,6 +2,7 @@ library(basedosdados)
 library(tidyverse)
 library(ggrepel)
 
+# Again using flamengadas project
 set_billing_id("flamengadas")
 
 # Obrigado, Base dos Dados, me poupou de um web scrapping
@@ -15,9 +16,13 @@ df <- basedosdados::read_sql("SELECT ano_campeonato, data, rodada, estadio,
 
 glimpse(df)
 
+# Breaks for fill scale
+my_breaks <- c(.5, 1, 1.5, 2, 3, 3.5)
+
 df %>%
-  filter(time_man != 'Flamengo',
-         publico > 0) %>%
+  filter(time_man != 'Flamengo', # Flamengo is not home
+         publico > 0) %>% # Excluding matches without supporters,
+  # mainly during pandemics
   mutate(ocupacao = publico/publico_max,
          VisFla = (time_vis == 'Flamengo')) %>%
   group_by(time_man, VisFla) %>%
@@ -27,10 +32,21 @@ df %>%
               names_from = VisFla,
               values_from = ocupacao_media) %>%
   ggplot(aes(x = `FALSE`, y = `TRUE`, fill = `TRUE`/`FALSE`)) +
-  geom_point(shape = 21) +
+  geom_point(shape = 21, size = 5) +
   geom_text_repel(aes(label = time_man)) +
   geom_abline(slope = 1, linetype = 'dashed') +
+  annotate("text", x = .75 + .25/2 + .025, y = .75 + .25/2 - .025,
+           label = "f(x) = x") +
   lims(x = c(0,1), y = c(0,1)) +
-  labs(x = 'Ocupação média sem Flamengo',
-       y = 'Ocupação média com Flamengo')
-
+  scale_fill_gradient(name = "Razão", trans = "log10",
+                      breaks = my_breaks, labels = my_breaks,
+                      low = "#f8d5d3", high = "#c3281e") +
+  scale_y_continuous(labels = scales::percent,
+                     name = 'Ocupação média com Flamengo') +
+  scale_x_continuous(labels = scales::percent,
+                     name = 'Ocupação média sem Flamengo') +
+  labs(title = 'Vimos pelo Flamengo!',
+       subtitle = paste0('Flamengo eleva ocupação média dos estádios','\n',
+       '*Período com restrições de capacidade pode afetar números')) +
+  theme_minimal()
+ggsave('attendance.png')
